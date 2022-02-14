7e3ced00446f9db9cec8063455ae1550384a2bfe
Gudduvaiya <arindampanda2001@gmail.com>
2022-02-14 12:53:08 +0530

    Version 2.0

--- App.js
+++ App.js
@@ -7,8 +7,13 @@ import {
   View,
   TextInput,
   Button,
+  Pressable,
   ScrollView,
   FlatList,
+  TouchableOpacity,
+  TouchableNativeFeedback,
+  TouchableHighlight,
+  Modal,
 } from "react-native";
 
 // import { Button, TextInput } from 'react-native-web';
@@ -16,35 +21,93 @@ import {
 export default function App() {
   const [inputGoal, setinputGoal] = useState("");
   const [outputGoal, setoutputGoal] = useState([]);
+  const [Modals, setModals] = useState(false);
   const Onpress = () => {
-    setoutputGoal((Currentgoal) => [...Currentgoal,{id: Math.random().toString(), value: inputGoal}]);
+    setoutputGoal((Currentgoal) => [
+      ...Currentgoal,
+      { id: Math.random().toString(), value: inputGoal },
+    ]);
+    setModals(false);
+    setinputGoal("");
+  };
+
+  const DeleteItem = (GoalId) => {
+    // if GoalId matches with goal.id then it will be deleted
+    setinputGoal((Currentgoals) => {
+      return Currentgoals.filter((goal) => goal.id !== GoalId);
+    });
   };
   return (
     <View style={style.root}>
-      <View style={style.wholeInputcontainer}>
-        <TextInput
-          placeholder="Enter the Text"
-          style={style.textInput}
-          onChangeText={(enteredtext) => {
-            setinputGoal(enteredtext);
-          }}
-          value={inputGoal}
-        />
-        <Button
-          title="Submit"
-          onPress={Onpress}
-        />
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <View style={style.ModalOnBtn}>
+          <Button
+            title="Add Note"
+            onPress={() => {
+              setModals(true);
+            }}
+            color="green"
+            style={style.ModalOnBtn}
+          />
+        </View>
+        <View style={style.ModalOnBtn}>
+          <Button
+            title="RESET"
+            onPress={() => {
+              setoutputGoal([]);
+            }}
+            color="red"
+            style={style.ModalOnBtn}
+          />
+        </View>
       </View>
+      <Modal visible={Modals} animationType="slide">
+        <View style={style.wholeInputcontainer}>
+          <TextInput
+            placeholder="Enter the Text"
+            style={style.textInput}
+            onChangeText={(enteredtext) => {
+              setinputGoal(enteredtext);
+            }}
+            value={inputGoal}
+          />
+          <View style={{ display: "flex", flexDirection: "row",  }}>
+            <View style={{width:100}}>
+              <Button title="Submit" onPress={Onpress} />
+            </View>
+            <View style={{width:100, marginLeft:13}}>
+              <Button
+                title="GO BACK"
+                color="red"
+                style={{ marginTop: 15 }}
+                onPress={() => {
+                  setModals(false);
+                }}
+              />
+            </View>
+          </View>
+        </View>
+      </Modal>
       {/* We can use mapping from outputGoal but using FlatList instead of ScrollView is more efficient */}
       <FlatList
-      keyExtractor={(item,index)=>item.id}
+        keyExtractor={(item, index) => item.id}
         data={outputGoal}
         renderItem={(listitems) => (
-          <View style={style.listView}>
-            <Text style={{ color: "green", fontWeight: "bold" }}>
-              {listitems.item.value}
-            </Text>
-          </View>
+          <TouchableNativeFeedback
+            onDelete={DeleteItem.bind(this, listitems.item.id)}
+          >
+            <View style={style.listView}>
+              <Text style={{ color: "green", fontWeight: "bold" }}>
+                {listitems.item.value}
+              </Text>
+            </View>
+          </TouchableNativeFeedback>
         )}
       />
       <StatusBar style="auto" />
@@ -54,7 +117,7 @@ export default function App() {
 const style = StyleSheet.create({
   //StyleSheet is a class of react-native, we can always use inline styleing
   root: {
-    marginTop: 100,
+    marginTop: 90,
   },
   listView: {
     width: 300,
@@ -68,6 +131,7 @@ const style = StyleSheet.create({
   },
   wholeInputcontainer: {
     display: "flex",
+    flex: 1,
     alignItems: "center",
     justifyContent: "center",
   },
@@ -76,5 +140,10 @@ const style = StyleSheet.create({
     borderBottomWidth: 4,
     marginBottom: 10,
     width: 300,
+  },
+  ModalOnBtn: {
+    borderRadius: 15,
+    width: 330,
+    marginTop:10
   },
 });
